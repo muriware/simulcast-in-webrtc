c5176384c9c2122e8d233086f81bf4694d59524b
Jose Murinello <josem@telnyx.com>
2022-05-23 10:06:26 +0900

    refactor: remove signaling from client

---
+++ client/js/draw-graphs.js
@@ -0,0 +1,147 @@
+// helper for graphs.
+const bitrateSeries = {};
+const bitrateGraphs = {};
+const framerateSeries = {};
+const framerateGraphs = {};
+let lastSendResult;
+let lastRecvResult;
+
+// Only for Firefox.
+let ssrc2track;
+
+// Show a stream and draw graphs.
+function show(stream, isRemote) {
+  const id = isRemote ? stream.id : 'local';
+
+  const container = document.createElement('div');
+  container.id = id + 'Container';
+  document
+    .getElementById(isRemote ? 'remotes' : 'local')
+    .appendChild(container);
+
+  const v = document.createElement('video');
+  v.autoplay = true;
+  v.srcObject = stream;
+  v.onresize = () =>
+    (v.title = 'video dimensions: ' + v.videoWidth + 'x' + v.videoHeight);
+  container.appendChild(v);
+
+  const bitrateCanvas = document.createElement('canvas');
+  bitrateCanvas.id = id + 'BitrateCanvas';
+  bitrateCanvas.title = 'Bitrate';
+  container.appendChild(bitrateCanvas);
+
+  const bitrateGraph = new TimelineGraphView(
+    id + 'Container',
+    id + 'BitrateCanvas'
+  );
+  bitrateGraph.updateEndDate();
+
+  bitrateSeries[id] = id === 'local' ? new Map() : new TimelineDataSeries();
+  bitrateGraphs[id] = bitrateGraph;
+
+  const framerateCanvas = document.createElement('canvas');
+  framerateCanvas.id = id + 'FramerateCanvas';
+  framerateCanvas.title = 'Framerate';
+  container.appendChild(framerateCanvas);
+
+  const framerateGraph = new TimelineGraphView(
+    id + 'Container',
+    id + 'FramerateCanvas'
+  );
+  framerateGraph.updateEndDate();
+
+  framerateSeries[id] = id === 'local' ? new Map() : new TimelineDataSeries();
+  framerateGraphs[id] = framerateGraph;
+}
+
+async function draw(pc1, pc2) {
+  const graphName = 'local';
+  const res1 = await pc1.getSenders()[0].getStats();
+  res1.forEach((report) => {
+    if (report.type !== 'outbound-rtp') return;
+    const now = report.timestamp;
+    const bytes = report.bytesSent;
+    const frames = report.framesEncoded;
+    if (lastSendResult && lastSendResult.get(report.id)) {
+      const ssrc = report.ssrc;
+
+      // calculate bitrate
+      const bitrate =
+        (8000 * (bytes - lastSendResult.get(report.id).bytesSent)) /
+        (now - lastSendResult.get(report.id).timestamp);
+      if (!bitrateSeries[graphName].has(ssrc)) {
+        const series = new TimelineDataSeries();
+        bitrateSeries[graphName].set(ssrc, series);
+        if (bitrateSeries[graphName].size === 2) {
+          series.setColor('green');
+        } else if (bitrateSeries[graphName].size === 3) {
+          series.setColor('blue');
+        }
+      }
+
+      if (bitrate >= 0) {
+        bitrateSeries[graphName].get(ssrc).addPoint(now, bitrate);
+      }
+
+      //  calculate framerate.
+      const framerate =
+        (1000 * (frames - lastSendResult.get(report.id).framesEncoded)) /
+        (now - lastSendResult.get(report.id).timestamp);
+      if (!framerateSeries[graphName].has(ssrc)) {
+        const series = new TimelineDataSeries();
+        framerateSeries[graphName].set(ssrc, series);
+        if (framerateSeries[graphName].size === 2) {
+          series.setColor('green');
+        } else if (framerateSeries[graphName].size === 3) {
+          series.setColor('blue');
+        }
+      }
+      if (framerate >= 0) {
+        framerateSeries[graphName].get(ssrc).addPoint(now, framerate);
+      }
+    }
+  });
+  bitrateGraphs[graphName].setDataSeries(
+    Array.from(bitrateSeries[graphName].values())
+  );
+  bitrateGraphs[graphName].updateEndDate();
+
+  framerateGraphs[graphName].setDataSeries(
+    Array.from(framerateSeries[graphName].values())
+  );
+  framerateGraphs[graphName].updateEndDate();
+  lastSendResult = res1;
+
+  const res2 = await pc2.getStats();
+  res2.forEach((report) => {
+    if (report.type !== 'inbound-rtp') return;
+    const now = report.timestamp;
+    const bytes = report.bytesReceived;
+    const frames = report.framesDecoded;
+    if (lastRecvResult && lastRecvResult.get(report.id)) {
+      let graphName = res2.get(report.trackId).trackIdentifier;
+      if (!bitrateSeries[graphName]) {
+        return;
+      }
+
+      // calculate bitrate
+      const bitrate =
+        (8000 * (bytes - lastRecvResult.get(report.id).bytesReceived)) /
+        (now - lastRecvResult.get(report.id).timestamp);
+
+      bitrateSeries[graphName].addPoint(now, bitrate);
+      bitrateGraphs[graphName].setDataSeries([bitrateSeries[graphName]]);
+      bitrateGraphs[graphName].updateEndDate();
+
+      //  calculate framerate.
+      const framerate =
+        (1000 * (frames - lastRecvResult.get(report.id).framesDecoded)) /
+        (now - lastRecvResult.get(report.id).timestamp);
+      framerateSeries[graphName].addPoint(now, framerate);
+      framerateGraphs[graphName].setDataSeries([framerateSeries[graphName]]);
+      framerateGraphs[graphName].updateEndDate();
+    }
+  });
+  lastRecvResult = res2;
+}
--- client/script.js
+++ client/script.js
@@ -1,99 +1,163 @@
-// Use WebSocket as a WebRTC Signaling channel
-class SignalingChannel extends WebSocket {
-  constructor() {
-    super('ws://localhost:4242');
-    this.listener = {};
-    this.addEventListener('message', this.emit);
-  }
+const pc1 = new RTCPeerConnection();
+const pc2 = new RTCPeerConnection();
+pc1.onicecandidate = (e) => pc2.addIceCandidate(e.candidate);
+pc2.onicecandidate = (e) => pc1.addIceCandidate(e.candidate);
+pc2.ontrack = (e) => show(e.streams[0], true);
 
-  set onmessage(callback) {
-    this.listener['message'] = callback;
-  }
+const extensionsToFilter = [
+  'urn:ietf:params:rtp-hdrext:sdes:mid',
+  'urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id',
+  'urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id',
+];
+const rids = [0, 1, 2];
 
-  emit(event) {
-    const callback = this.listener[event.type];
-    if (typeof callback === 'function') {
-      callback({ data: JSON.parse(event.data) });
-    }
-  }
+document.getElementById('enable-camera').addEventListener('click', start);
 
-  send(data) {
-    super.send(JSON.stringify(data));
-  }
-}
+function start() {
+  navigator.mediaDevices
+    .getUserMedia({ video: { width: 1280, height: 720 } })
+    .then((stream) => {
+      pc1.addTransceiver(stream.getVideoTracks()[0], {
+        streams: [stream],
+        sendEncodings: [
+          { rid: 0, active: true },
+          { rid: 1, active: true },
+          { rid: 2, active: true },
+        ],
+      });
+      show(stream, false);
+      return pc1.createOffer();
+    })
+    .then((offer) => {
+      const sections = SDPUtils.splitSections(offer.sdp);
+      const dtls = SDPUtils.getDtlsParameters(sections[1], sections[0]);
+      const ice = SDPUtils.getIceParameters(sections[1], sections[0]);
+      const rtpParameters = SDPUtils.parseRtpParameters(sections[1]);
+      const rtcpParameters = SDPUtils.parseRtcpParameters(sections[1]);
 
-const signaling = new SignalingChannel(); // handles JSON.stringify/parse
-const constraints = { video: true };
-const configuration = {
-  iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
-};
-const pc = new RTCPeerConnection(configuration);
+      // The gist of this hack is that rid and mid have the same wire format.
+      // Kudos to orphis for this clever hack!
+      const rid = rtpParameters.headerExtensions.find(
+        (ext) => ext.uri === 'urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id'
+      );
+      rtpParameters.headerExtensions = rtpParameters.headerExtensions.filter(
+        (ext) => {
+          return !extensionsToFilter.includes(ext.uri);
+        }
+      );
+      // This tells the other side that the RID packets are actually mids.
+      rtpParameters.headerExtensions.push({
+        id: rid.id,
+        uri: 'urn:ietf:params:rtp-hdrext:sdes:mid',
+        direction: 'sendrecv',
+      });
 
-const selfView = document.getElementById('self-view');
-const remoteView = document.getElementById('remote-view');
-const cameraButton = document.getElementById('enable-camera');
+      // Filter rtx as we have no wait to reinterpret rrid. Not doing this makes probing use RTX, its not understood
+      // and ramp-up is slower.
+      rtpParameters.codecs = rtpParameters.codecs.filter(
+        (c) => c.name.toUpperCase() !== 'RTX'
+      );
 
-// Send any ice candidates to the other peer
-pc.onicecandidate = ({ candidate }) => signaling.send({ candidate });
+      let sdp =
+        SDPUtils.writeSessionBoilerplate() +
+        SDPUtils.writeDtlsParameters(dtls, 'actpass') +
+        SDPUtils.writeIceParameters(ice) +
+        'a=group:BUNDLE 0 1 2\r\n' +
+        'a=msid-semantic:WMS *\r\n';
+      const codecs =
+        SDPUtils.writeRtpDescription('video', rtpParameters) +
+        SDPUtils.writeRtcpParameters({
+          mux: rtcpParameters.mux,
+          reducedSize: rtcpParameters.reducedSize,
+        });
+      sdp +=
+        codecs +
+        'a=setup:actpass\r\n' +
+        'a=mid:' +
+        rids[0] +
+        '\r\n' +
+        'a=msid:low low\r\n';
+      sdp +=
+        codecs +
+        'a=setup:actpass\r\n' +
+        'a=mid:' +
+        rids[1] +
+        '\r\n' +
+        'a=msid:mid mid\r\n';
+      sdp +=
+        codecs +
+        'a=setup:actpass\r\n' +
+        'a=mid:' +
+        rids[2] +
+        '\r\n' +
+        'a=msid:hi hi\r\n';
 
-// Let the "negotiationneeded" event trigger offer generation
-pc.onnegotiationneeded = async () => {
-  try {
-    await pc.setLocalDescription();
-    // Send the offer to the other peer
-    signaling.send({ description: pc.localDescription });
-  } catch (err) {
-    console.error(err);
-  }
-};
+      return Promise.all([
+        pc1.setLocalDescription(offer),
+        pc2.setRemoteDescription({
+          type: 'offer',
+          sdp,
+        }),
+      ]);
+    })
+    .then(() => pc2.createAnswer())
+    .then((answer) => {
+      const sections = SDPUtils.splitSections(answer.sdp);
+      const dtls = SDPUtils.getDtlsParameters(sections[1], sections[0]);
+      const ice = SDPUtils.getIceParameters(sections[1], sections[0]);
+      const rtpParameters = SDPUtils.parseRtpParameters(sections[1]);
+      const rtcpParameters = SDPUtils.parseRtcpParameters(sections[1]);
+      // Avoid duplicating the mid extension even though Chrome does not care (boo!)
+      rtpParameters.headerExtensions = rtpParameters.headerExtensions.filter(
+        (ext) => {
+          return !extensionsToFilter.includes(ext.uri);
+        }
+      );
+      let sdp =
+        SDPUtils.writeSessionBoilerplate() +
+        SDPUtils.writeDtlsParameters(dtls, 'active') +
+        SDPUtils.writeIceParameters(ice) +
+        'a=group:BUNDLE 0\r\n' +
+        'a=msid-semantic:WMS *\r\n';
+      const codecs =
+        SDPUtils.writeRtpDescription('video', rtpParameters) +
+        SDPUtils.writeRtcpParameters({
+          mux: rtcpParameters.mux,
+          reducedSize: rtcpParameters.reducedSize,
+        });
+      sdp += codecs;
+      sdp += 'a=setup:active\r\n';
 
-pc.ontrack = ({ track, transceiver }) => {
-  // Once media for the remote track arrives, show it in a remote-view
-  track.onunmute = () => {
-    // Don't set srcObject again if it is already set
-    if (!remoteView.srcObject) {
-      remoteView.srcObject = new MediaStream();
-    }
-    remoteView.srcObject.addTrack(track);
-  };
-};
+      rids.forEach((rid) => {
+        sdp += 'a=rid:' + rid + ' recv\r\n';
+      });
+      sdp += 'a=simulcast:recv ' + rids.join(';') + '\r\n';
+      sdp +=
+        'a=mid:' +
+        SDPUtils.getMid(SDPUtils.splitSections(pc1.localDescription.sdp)[1]) +
+        '\r\n';
 
-signaling.onmessage = async ({ data: { description, candidate } }) => {
-  try {
-    if (description) {
-      await pc.setRemoteDescription(description);
-      // If we got an offer, we need to reply with an answer
-      if (description.type === 'offer') {
-        await pc.setLocalDescription();
-        signaling.send({ description: pc.localDescription });
-      }
-    }
-
-    if (candidate) {
-      await pc.addIceCandidate(candidate);
-    }
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-cameraButton.addEventListener('click', addCamera);
-
-async function addCamera() {
-  try {
-    // Get a local stream, show it in a self-view and add it to be sent
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    selfView.srcObject = stream;
-    pc.addTransceiver(stream.getVideoTracks()[0], {
-      direction: 'sendonly',
-      // Establish the simulcast envelope on the transceiver
-      sendEncodings: [
-        { rid: 'q', active: true, scaleResolutionDownBy: 4.0 },
-        { rid: 'h', active: true, scaleResolutionDownBy: 2.0 },
-        { rid: 'f', active: true },
-      ],
-    });
-  } catch (err) {
-    console.error(err);
-  }
+      // Re-add headerextensions we filtered.
+      const headerExtensions = SDPUtils.parseRtpParameters(
+        SDPUtils.splitSections(pc1.localDescription.sdp)[1]
+      ).headerExtensions;
+      headerExtensions.forEach((ext) => {
+        if (extensionsToFilter.includes(ext.uri)) {
+          sdp += 'a=extmap:' + ext.id + ' ' + ext.uri + '\r\n';
+        }
+      });
+      return Promise.all([
+        pc2.setLocalDescription(answer),
+        pc1.setRemoteDescription({
+          type: 'answer',
+          sdp,
+        }),
+      ]);
+    })
+    .then(() => {
+      window.setInterval(() => {
+        draw(pc1, pc2);
+      }, 2000);
+    })
+    .catch((e) => console.error(e));
 }
